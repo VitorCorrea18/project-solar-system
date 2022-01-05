c6ac3e85ca314c1a5e5db634fe732104d08b06f6
vitorcorrea18 <vitorcorrea18@gmail.com>
2022-01-04 21:08:42 -0400

    correcao lint

--- src/components/MissionCard.js
+++ src/components/MissionCard.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React from 'react';
 import PropTypes from 'prop-types';
 
 class MissionCard extends React.Component {
@@ -11,7 +11,7 @@ class MissionCard extends React.Component {
         <span data-testid="mission-country">{country}</span>
         <span data-testid="mission-destination">{destination}</span>
       </div>
-        );
+    );
   }
 }
 
@@ -20,6 +20,6 @@ MissionCard.propTypes = {
   year: PropTypes.string.isRequired,
   country: PropTypes.string.isRequired,
   destination: PropTypes.string.isRequired,
-}
+};
 
 export default MissionCard;
--- src/components/Missions.js
+++ src/components/Missions.js
@@ -6,16 +6,16 @@ import missions from '../data/missions';
 class Missions extends React.Component {
   render() {
     return (
-      <div className='missions' data-testid="missions">
+      <div className="missions" data-testid="missions">
         <Title headline="Missões" />
-        <div className='missions-section'>
+        <div className="missions-section">
           {missions.map((mission) => (
             <MissionCard
-            key={mission.name}
-            name={mission.name}
-            year={mission.year}
-            country={mission.country}
-            destination={mission.destination}
+              key={ mission.name }
+              name={ mission.name }
+              year={ mission.year }
+              country={ mission.country }
+              destination={ mission.destination }
             />
           ))}
         </div>
